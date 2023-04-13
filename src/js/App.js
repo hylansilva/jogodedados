59405a086c2f3a395d156ec9fbb769d27be963ff
Hylan Silva <hylan.00000847839@unicap.br>
2023-04-13 17:23:51 -0300

    ✨ feat: função de declarar o vencedor final

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,14 @@
 // definindo as voariaveis do jogo 
 let scoreboard = []; // criando um array para salvar os dados dos jogadores
-const rounds = 10; // criando a quantidade de rounds 
-let turne = 2; // definindo quantos turnos haverão no jogo
+let rounds = 1; // criando a quantidade de rounds 
+let turn = 2; // definindo quantos turnos haverão no jogo
 const side = 6; // definindo até qual tamanho deve ser o sorteido dos valores
 let partialPlayerOneValue;
 let partialPlayerTwoValue;
+let winer;
 
 // Buscando o elemento body do html-
-const body = document.getElementsByTagName(HTMLBodyElement);
+const body = document.getElementsByTagName("body");
 
 // criando a div principal do jogo que mostra os dados
 const main = document.createElement("div");
@@ -64,8 +65,7 @@ const sortDieOne = () =>{
     hndButtonRestart.disabled = false;
     hndButtonPlayerTwo.disabled = false;
     partialPlayerOneValue = sort;
-    turne--;
-    partialResult(partialPlayerOneValue, partialPlayerTwoValue);
+    turn--;
 };
 
 // função de sortear o dado 2
@@ -76,7 +76,7 @@ const sortDieTwo = () =>{
     hndButtonPlayerOne.disabled = false;
     hndButtonPlayerTwo.disabled = true;
     partialPlayerTwoValue = sort
-    turne--;
+    turn--;
     partialResult(partialPlayerOneValue, partialPlayerTwoValue);
 };
 
@@ -89,42 +89,114 @@ const restart = () =>{
     hndButtonPlayerOne.disabled = false;
     partialPlayerOneValue = 0;
     partialPlayerTwoValue = 0;
-    turne = 2;
+    turn = 2;
+    scoreboard = [];
 };
+
+const newRund = () =>{
+    rounds = rounds + 1;
+    console.log(scoreboard);
+    socrePlayerOne.innerText = "";
+    socrePlayerTwo.innerText = "";
+    hndButtonRestart.disabled = false;
+    hndButtonPlayerTwo.disabled = true;
+    hndButtonPlayerOne.disabled = false;
+    partialPlayerOneValue = 0;
+    partialPlayerTwoValue = 0;
+    turn = 2;
+}
 
 // função que compara os resultados parciais
 const partialResult = (valueOne, valueTwo) =>{
-    if(turne == 0){
+    
+    // Condição para saber a vitória ou empate 
+    if(turn == 0){
         if(valueOne > valueTwo){
-            hndButtonRestart.disabled = true;
-            hndButtonPlayerTwo.disabled = true;
-            hndButtonPlayerOne.disabled = true;
-            setTimeout(()=>{
-                alert('Jogador 1 venceu');
-                restart();
-            },700);
-        }else if(valueOne < valueTwo){
-            hndButtonRestart.disabled = true;
-            hndButtonPlayerTwo.disabled = true;
-            hndButtonPlayerOne.disabled = true;
-            setTimeout(()=>{
-                alert('Jogador 2 venceu');
-                restart();
-            },700);
-        }else{
-            hndButtonRestart.disabled = true;
-            hndButtonPlayerTwo.disabled = true;
-            hndButtonPlayerOne.disabled = true;
-            setTimeout(()=>{
-                alert('Houve um Empate');
-                restart();
-            },700);
+            if(rounds < 11){
+                hndButtonRestart.disabled = true;
+                hndButtonPlayerTwo.disabled = true;
+                hndButtonPlayerOne.disabled = true;
+                winer = 2
+                setTimeout(()=>{
+                    alert(`Jogador 2 venceu a rodada ${rounds}`);
+                        if(rounds == 10){
+                            finalResult();
+                        }else{
+                            newRund();
+                        }
+                },700);
+                
+            }
+        }
+        else if(valueOne < valueTwo){
+            if(rounds < 11){
+                hndButtonRestart.disabled = true;
+                hndButtonPlayerTwo.disabled = true;
+                hndButtonPlayerOne.disabled = true;
+                winer = 2
+                setTimeout(()=>{
+                    alert(`Jogador 2 venceu a rodada ${rounds}`);
+                        if(rounds == 10){
+                            finalResult();
+                        }else{
+                            newRund();
+                        }
+                },700);
+            }
+            
+
+        }
+        else{
+            if(rounds < 11){
+                hndButtonRestart.disabled = true;
+                hndButtonPlayerTwo.disabled = true;
+                hndButtonPlayerOne.disabled = true;
+                winer = 0
+                setTimeout(()=>{
+                    alert(`Ambos venceram a rodada ${rounds}`);
+                        if(rounds == 10){
+                            finalResult();
+                        }else{
+                            newRund();
+                        }
+                },700);
+        }
+        
+    };
+
+        // objeto que define os dados da rodada no array do resultado final
+        const roudStats = {
+            round:rounds,
+            winer: winer
+        };
+        // mandando o objeto para dentro do array
+        scoreboard.push(roudStats);
+    };
+};
+// função que compara o resultado total do jogo
+const finalResult = () =>{
+     // calculando a pontuação total dos jogadores
+    let totalScorePlayerOne = 0;
+    let totalScorePlayerTwo = 0;
+
+    for (let i = 0; i < scoreboard.length; i++) {
+        if (scoreboard[i].winer === 1) {
+            totalScorePlayerOne++;
+        } else if (scoreboard[i].winer === 2) {
+            totalScorePlayerTwo++;
         }
     }
+
+  // Declarando o vencedor final
+    if (totalScorePlayerOne > totalScorePlayerTwo) {
+        alert(`Jogador 1 venceu o jogo com ${totalScorePlayerOne} rounds ganhos.`);
+    } else if (totalScorePlayerOne < totalScorePlayerTwo) {
+        alert(`Jogador 2 venceu o jogo com ${totalScorePlayerTwo} rounds ganhos.`);
+    } else {
+        alert(`O jogo terminou em empate com ${totalScorePlayerOne} rounds ganhos para cada jogador.`);
+    }
+    restart();
 };
-
-// função
-
 // Atribuindo as funções aos botões
 hndButtonPlayerTwo.onclick = sortDieTwo;
 hndButtonPlayerOne.onclick = sortDieOne;
