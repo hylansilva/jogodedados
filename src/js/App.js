0c187599acad55b65e0622e25c44cb6123d914f8
hylan.00000847839@unicap.br <aluno@unicap.br>
2023-04-14 20:07:14 -0300

    💄 + ♻️ feat: styling css and refator the modal

--- src/js/App.js
+++ src/js/App.js
@@ -12,18 +12,55 @@ const body = document.getElementsByTagName("body");
 
 // criando uma div para organizar os elementos
 const game = document.createElement("div");
-game.classList = "game"
-document.body.appendChild(game)
+game.classList = "game";
+document.body.appendChild(game);
 
 // criando a div principal do jogo que mostra os dados
 const main = document.createElement("div");
 main.className = "pronpt";
-game.appendChild(main)
+game.appendChild(main);
 
 // criando uma div para armazenar a tabela com os resultados
 const divTable = document.createElement("div");
-divTable.className = "divTable"
+divTable.className = "divTable";
 document.body.appendChild(divTable);
+
+const showModal = (title, message) => {
+    // create the modal container
+    const modalContainer = document.createElement('div');
+    modalContainer.classList.add('modal-container');
+
+    // create the modal content
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+
+    // create the title and message elements
+    const modalTitle = document.createElement('h2');
+    modalTitle.textContent = title;
+    const modalMessage = document.createElement('p');
+    modalMessage.textContent = message;
+
+    // create the button to close the modal
+    const closeButton = document.createElement('button');
+    closeButton.classList.add('modal-close');
+    closeButton.textContent = 'FECHAR';
+
+    // add the title, message, and close button to the modal content
+    modalContent.appendChild(modalTitle);
+    modalContent.appendChild(modalMessage);
+    modalContent.appendChild(closeButton);
+
+    // add the modal content to the modal container
+    modalContainer.appendChild(modalContent);
+
+    // add the modal container to the document body
+    document.body.appendChild(modalContainer);
+
+    // add event listener to close button to remove the modal from the DOM when clicked
+    closeButton.addEventListener('click', () => {
+    modalContainer.remove();
+    });
+};
 
 // criando a tabela de resultados
 const resultTable = document.createElement("table");
@@ -62,32 +99,32 @@ const socrePlayerOne = document.createElement("p");
 socrePlayerOne.innerHTML = "0";
 outputPlayerOne.className = "player-one";
 main.appendChild(outputPlayerOne);
-socrePlayerOne.className = "sort-text"
+socrePlayerOne.className = "sort-text";
 // lado 2
 const outputPlayerTwo = document.createElement("div");
 const socrePlayerTwo = document.createElement("p");
 socrePlayerTwo.innerHTML = "0";
 outputPlayerTwo.className = "player-two";
 main.appendChild(outputPlayerTwo);
-socrePlayerTwo.className = "sort-text"
+socrePlayerTwo.className = "sort-text";
 
 // criando os botões do jogo
 // botão do player 1
 const hndButtonPlayerOne = document.createElement("button");
 hndButtonPlayerOne.innerText = "Jogador 1";
-hndButtonPlayerOne.className = "button"
+hndButtonPlayerOne.className = "button";
 
 
 // botão de reiniciar
 const hndButtonRestart = document.createElement("button");
 hndButtonRestart.innerText = "Reinicar o Jogo";
-hndButtonRestart.className = "button"
+hndButtonRestart.className = "button";
 
 
 // botão do player 2
 const hndButtonPlayerTwo = document.createElement("button");
 hndButtonPlayerTwo.innerText = "Jogador 2";
-hndButtonPlayerTwo.className = "button"
+hndButtonPlayerTwo.className = "button";
 
 // criando a div que adiciona os botões em um unico form
 const control = document.createElement("div");
@@ -96,7 +133,7 @@ control.appendChild(hndButtonPlayerOne);
 control.appendChild(hndButtonRestart);
 control.appendChild(hndButtonPlayerTwo);
 document.body.appendChild(control);
-game.appendChild(control)
+game.appendChild(control);
 
 // desabilitando os botões de player 2 e restart
 hndButtonPlayerTwo.disabled = true;
@@ -140,8 +177,8 @@ const newTableLine = (scoreboard) => {
         trBody.appendChild(lineRound);
         
         resulTableBody.appendChild(trBody);
-    }
-}
+    };
+};
 
 
 // função de sortear o dado 1
@@ -180,7 +217,7 @@ const restart = () =>{
     turn = 2;
     scoreboard = [];
     rounds = 1;
-    resulTableBody.innerHTML = ""
+    resulTableBody.innerHTML = "";
 };
 
 const newTurn = () =>{
@@ -199,7 +236,6 @@ const newTurn = () =>{
 
 // função que compara os resultados parciais
 const partialResult = (valueOne, valueTwo) =>{
-    
     // Condição para saber a vitória ou empate 
     if(turn == 0){
         if(valueOne > valueTwo){
@@ -207,9 +243,9 @@ const partialResult = (valueOne, valueTwo) =>{
                 hndButtonRestart.disabled = true;
                 hndButtonPlayerTwo.disabled = true;
                 hndButtonPlayerOne.disabled = true;
-                winer = 1
+                winer = 1;
                 setTimeout(()=>{
-                    alert(`Jogador 1 venceu a rodada ${rounds}`);
+                    showModal('Vitória',`Jogador 1 venceu a rodada ${rounds}`);
                         if(rounds == 10){
                             finalResult();
                         }else{
@@ -224,9 +260,9 @@ const partialResult = (valueOne, valueTwo) =>{
                 hndButtonRestart.disabled = true;
                 hndButtonPlayerTwo.disabled = true;
                 hndButtonPlayerOne.disabled = true;
-                winer = 2
+                winer = 2;
                 setTimeout(()=>{
-                    alert(`Jogador 2 venceu a rodada ${rounds}`);
+                    showModal('Vitória',`Jogador 2 venceu a rodada ${rounds}`);
                         if(rounds == 10){
                             finalResult();
                         }else{
@@ -235,16 +271,15 @@ const partialResult = (valueOne, valueTwo) =>{
                 },700);
             }
             
-
         }
         else{
             if(rounds < 11){
                 hndButtonRestart.disabled = true;
                 hndButtonPlayerTwo.disabled = true;
                 hndButtonPlayerOne.disabled = true;
-                winer = 0
+                winer = 0;
                 setTimeout(()=>{
-                    alert(`Ambos venceram a rodada ${rounds}`);
+                    showModal('Empate !',`Ambos venceram a rodada ${rounds}`);
                         if(rounds == 10){
                             finalResult();
                         }else{
@@ -254,7 +289,6 @@ const partialResult = (valueOne, valueTwo) =>{
         }
         
     };
-
         // objeto que define os dados da rodada no array do resultado final
         const roudStats = {
             round:rounds,
@@ -282,11 +316,11 @@ const finalResult = () =>{
 
   // Declarando o vencedor final
     if (totalScorePlayerOne > totalScorePlayerTwo) {
-        alert(`Jogador 1 venceu o jogo com ${totalScorePlayerOne} rounds ganhos.`);
+        showModal('Vitória',`Jogador 1 venceu o jogo com ${totalScorePlayerOne} rounds ganhos.`);
     } else if (totalScorePlayerOne < totalScorePlayerTwo) {
-        alert(`Jogador 2 venceu o jogo com ${totalScorePlayerTwo} rounds ganhos.`);
+        showModal('Vitória',`Jogador 2 venceu o jogo com ${totalScorePlayerTwo} rounds ganhos.`);
     } else {
-        alert(`O jogo terminou em empate com ${totalScorePlayerOne} rounds ganhos para cada jogador.`);
+        showModal('Empate   ',`O jogo terminou em empate com ${totalScorePlayerOne} rounds ganhos para cada jogador.`);
     }
     restart();
 };
